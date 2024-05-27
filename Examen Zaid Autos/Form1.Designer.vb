1dd11776bf7ae7b6b7e6e4f696f5b3000831fd6a
zaid <zaidrod08@gmail.com>
2024-05-27 23:06:21 +0200

    Carga de los ficheros a gitHub

---
+++ Examen Zaid Autos/Form1.Designer.vb
@@ -0,0 +1,352 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ComboMarca = New System.Windows.Forms.ComboBox()
+        Me.ListBoxModelos = New System.Windows.Forms.ListBox()
+        Me.PictureBoxCentro = New System.Windows.Forms.PictureBox()
+        Me.TextBoxCC = New System.Windows.Forms.TextBox()
+        Me.TextBoxPrecio = New System.Windows.Forms.TextBox()
+        Me.TextBoxCV = New System.Windows.Forms.TextBox()
+        Me.TextBoxPromocion = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButtonElec = New System.Windows.Forms.RadioButton()
+        Me.RadioButtonHibri = New System.Windows.Forms.RadioButton()
+        Me.RadioButtonGaso = New System.Windows.Forms.RadioButton()
+        Me.ListBoxPedidos = New System.Windows.Forms.ListBox()
+        Me.ButtonRealiza = New System.Windows.Forms.Button()
+        Me.ButtonBorrarP = New System.Windows.Forms.Button()
+        Me.ButtonVerPedi = New System.Windows.Forms.Button()
+        Me.ButtonSalir = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        CType(Me.PictureBoxCentro, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(49, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Marca"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(181, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Modelo"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(528, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Caracteristicas"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(748, 25)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Propulsión"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(66, 269)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(44, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "pedidos"
+        '
+        'ComboMarca
+        '
+        Me.ComboMarca.FormattingEnabled = True
+        Me.ComboMarca.Items.AddRange(New Object() {"BMW", "KIA", "RENAULT"})
+        Me.ComboMarca.Location = New System.Drawing.Point(21, 57)
+        Me.ComboMarca.Name = "ComboMarca"
+        Me.ComboMarca.Size = New System.Drawing.Size(106, 21)
+        Me.ComboMarca.TabIndex = 5
+        '
+        'ListBoxModelos
+        '
+        Me.ListBoxModelos.FormattingEnabled = True
+        Me.ListBoxModelos.Location = New System.Drawing.Point(149, 50)
+        Me.ListBoxModelos.Name = "ListBoxModelos"
+        Me.ListBoxModelos.Size = New System.Drawing.Size(126, 108)
+        Me.ListBoxModelos.TabIndex = 6
+        '
+        'PictureBoxCentro
+        '
+        Me.PictureBoxCentro.Location = New System.Drawing.Point(281, 50)
+        Me.PictureBoxCentro.Name = "PictureBoxCentro"
+        Me.PictureBoxCentro.Size = New System.Drawing.Size(152, 101)
+        Me.PictureBoxCentro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxCentro.TabIndex = 7
+        Me.PictureBoxCentro.TabStop = False
+        '
+        'TextBoxCC
+        '
+        Me.TextBoxCC.Location = New System.Drawing.Point(465, 57)
+        Me.TextBoxCC.Name = "TextBoxCC"
+        Me.TextBoxCC.Size = New System.Drawing.Size(93, 20)
+        Me.TextBoxCC.TabIndex = 8
+        '
+        'TextBoxPrecio
+        '
+        Me.TextBoxPrecio.Location = New System.Drawing.Point(465, 104)
+        Me.TextBoxPrecio.Name = "TextBoxPrecio"
+        Me.TextBoxPrecio.Size = New System.Drawing.Size(93, 20)
+        Me.TextBoxPrecio.TabIndex = 9
+        '
+        'TextBoxCV
+        '
+        Me.TextBoxCV.Location = New System.Drawing.Point(578, 58)
+        Me.TextBoxCV.Name = "TextBoxCV"
+        Me.TextBoxCV.Size = New System.Drawing.Size(93, 20)
+        Me.TextBoxCV.TabIndex = 10
+        '
+        'TextBoxPromocion
+        '
+        Me.TextBoxPromocion.Location = New System.Drawing.Point(578, 104)
+        Me.TextBoxPromocion.Name = "TextBoxPromocion"
+        Me.TextBoxPromocion.Size = New System.Drawing.Size(93, 20)
+        Me.TextBoxPromocion.TabIndex = 11
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButtonGaso)
+        Me.GroupBox1.Controls.Add(Me.RadioButtonHibri)
+        Me.GroupBox1.Controls.Add(Me.RadioButtonElec)
+        Me.GroupBox1.Location = New System.Drawing.Point(723, 59)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 121)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'RadioButtonElec
+        '
+        Me.RadioButtonElec.AutoSize = True
+        Me.RadioButtonElec.Location = New System.Drawing.Point(29, 26)
+        Me.RadioButtonElec.Name = "RadioButtonElec"
+        Me.RadioButtonElec.Size = New System.Drawing.Size(66, 17)
+        Me.RadioButtonElec.TabIndex = 0
+        Me.RadioButtonElec.TabStop = True
+        Me.RadioButtonElec.Text = "Electrico"
+        Me.RadioButtonElec.UseVisualStyleBackColor = True
+        '
+        'RadioButtonHibri
+        '
+        Me.RadioButtonHibri.AutoSize = True
+        Me.RadioButtonHibri.Location = New System.Drawing.Point(29, 49)
+        Me.RadioButtonHibri.Name = "RadioButtonHibri"
+        Me.RadioButtonHibri.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButtonHibri.TabIndex = 1
+        Me.RadioButtonHibri.TabStop = True
+        Me.RadioButtonHibri.Text = "Hibrido"
+        Me.RadioButtonHibri.UseVisualStyleBackColor = True
+        '
+        'RadioButtonGaso
+        '
+        Me.RadioButtonGaso.AutoSize = True
+        Me.RadioButtonGaso.Location = New System.Drawing.Point(29, 75)
+        Me.RadioButtonGaso.Name = "RadioButtonGaso"
+        Me.RadioButtonGaso.Size = New System.Drawing.Size(66, 17)
+        Me.RadioButtonGaso.TabIndex = 2
+        Me.RadioButtonGaso.TabStop = True
+        Me.RadioButtonGaso.Text = "Gasolina"
+        Me.RadioButtonGaso.UseVisualStyleBackColor = True
+        '
+        'ListBoxPedidos
+        '
+        Me.ListBoxPedidos.FormattingEnabled = True
+        Me.ListBoxPedidos.Location = New System.Drawing.Point(24, 295)
+        Me.ListBoxPedidos.Name = "ListBoxPedidos"
+        Me.ListBoxPedidos.Size = New System.Drawing.Size(125, 134)
+        Me.ListBoxPedidos.TabIndex = 13
+        '
+        'ButtonRealiza
+        '
+        Me.ButtonRealiza.Location = New System.Drawing.Point(228, 282)
+        Me.ButtonRealiza.Name = "ButtonRealiza"
+        Me.ButtonRealiza.Size = New System.Drawing.Size(124, 23)
+        Me.ButtonRealiza.TabIndex = 14
+        Me.ButtonRealiza.Text = "Realizar pedido"
+        Me.ButtonRealiza.UseVisualStyleBackColor = True
+        '
+        'ButtonBorrarP
+        '
+        Me.ButtonBorrarP.Location = New System.Drawing.Point(228, 326)
+        Me.ButtonBorrarP.Name = "ButtonBorrarP"
+        Me.ButtonBorrarP.Size = New System.Drawing.Size(124, 23)
+        Me.ButtonBorrarP.TabIndex = 15
+        Me.ButtonBorrarP.Text = "Borrar pedido"
+        Me.ButtonBorrarP.UseVisualStyleBackColor = True
+        '
+        'ButtonVerPedi
+        '
+        Me.ButtonVerPedi.Location = New System.Drawing.Point(227, 370)
+        Me.ButtonVerPedi.Name = "ButtonVerPedi"
+        Me.ButtonVerPedi.Size = New System.Drawing.Size(125, 23)
+        Me.ButtonVerPedi.TabIndex = 16
+        Me.ButtonVerPedi.Text = "Ver pedido"
+        Me.ButtonVerPedi.UseVisualStyleBackColor = True
+        '
+        'ButtonSalir
+        '
+        Me.ButtonSalir.Location = New System.Drawing.Point(251, 400)
+        Me.ButtonSalir.Name = "ButtonSalir"
+        Me.ButtonSalir.Size = New System.Drawing.Size(76, 38)
+        Me.ButtonSalir.TabIndex = 17
+        Me.ButtonSalir.Text = "Salir"
+        Me.ButtonSalir.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(508, 41)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(21, 13)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "CC"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(599, 42)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(21, 13)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "CV"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(492, 87)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(37, 13)
+        Me.Label8.TabIndex = 20
+        Me.Label8.Text = "Precio"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(599, 88)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(57, 13)
+        Me.Label9.TabIndex = 21
+        Me.Label9.Text = "Promocion"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(327, 26)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(54, 13)
+        Me.Label10.TabIndex = 22
+        Me.Label10.Text = "Fotografia"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(945, 450)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.ButtonSalir)
+        Me.Controls.Add(Me.ButtonVerPedi)
+        Me.Controls.Add(Me.ButtonBorrarP)
+        Me.Controls.Add(Me.ButtonRealiza)
+        Me.Controls.Add(Me.ListBoxPedidos)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBoxPromocion)
+        Me.Controls.Add(Me.TextBoxCV)
+        Me.Controls.Add(Me.TextBoxPrecio)
+        Me.Controls.Add(Me.TextBoxCC)
+        Me.Controls.Add(Me.PictureBoxCentro)
+        Me.Controls.Add(Me.ListBoxModelos)
+        Me.Controls.Add(Me.ComboMarca)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBoxCentro, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboMarca As ComboBox
+    Friend WithEvents ListBoxModelos As ListBox
+    Friend WithEvents PictureBoxCentro As PictureBox
+    Friend WithEvents TextBoxCC As TextBox
+    Friend WithEvents TextBoxPrecio As TextBox
+    Friend WithEvents TextBoxCV As TextBox
+    Friend WithEvents TextBoxPromocion As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButtonGaso As RadioButton
+    Friend WithEvents RadioButtonHibri As RadioButton
+    Friend WithEvents RadioButtonElec As RadioButton
+    Friend WithEvents ListBoxPedidos As ListBox
+    Friend WithEvents ButtonRealiza As Button
+    Friend WithEvents ButtonBorrarP As Button
+    Friend WithEvents ButtonVerPedi As Button
+    Private WithEvents ButtonSalir As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+End Class
---
+++ Examen Zaid Autos/Form1.vb
@@ -0,0 +1,314 @@
+﻿Public Class Form1
+
+    Private Structure Coche
+        Public modelo As String
+        Public CC As String
+        Public CV As String
+        Public promocion As Integer
+        Public precio As Integer
+
+    End Structure
+
+
+    Private Structure Pedidos
+        Public pedidoMarca As String
+        Public pedidoModelo As String
+        Public numPedido As Integer
+
+    End Structure
+    ' Creo el listado de los pedidos
+    Dim listadoPedidos(5) As Pedidos
+
+    ' Almaceno la marca seleccionada en el combobox
+    Dim Marca As String
+
+
+    ' Creo el listado de los coches
+    Dim listadoBMW(3) As Coche
+    Dim listadoKIA(2) As Coche
+    Dim listadoRENAULT(3) As Coche
+    ' Guardo el coche seleccionado
+    Dim cocheSeleccionado As Coche
+
+    'Indice que guarda la cantidad del pedio
+    Dim indice As Integer = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Private Sub ComboMarca_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboMarca.SelectedIndexChanged
+        ' Borro el contenido del listbox para cargar un nuevo grupo de coches
+        ListBoxModelos.Items.Clear()
+
+        Select Case ComboMarca.SelectedItem
+            ' Filtro el funcion de la marca y selecciono el listado a mostrar
+            Case "BMW"
+
+                For i As Integer = 0 To 2
+                    ListBoxModelos.Items.Add(listadoBMW(i).modelo)
+                Next
+
+                'Almaceno la marca
+                Marca = "BMW"
+
+            Case "KIA"
+                For i As Integer = 0 To 1
+                    ListBoxModelos.Items.Add(listadoKIA(i).modelo)
+                Next
+
+                'Almaceno la marca
+                Marca = "KIA"
+
+
+            Case "RENAULT"
+
+                For i As Integer = 0 To 2
+                    ListBoxModelos.Items.Add(listadoRENAULT(i).modelo)
+                Next
+
+                'Almaceno la marca
+                Marca = "RENAULT"
+
+        End Select
+
+
+
+
+
+    End Sub
+
+    ' Creo el listado de todos los coches y los agrego a sus listas
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim cocheK1 As Coche
+        cocheK1.modelo = "X1"
+        cocheK1.CC = "2"
+        cocheK1.CV = "3"
+        cocheK1.precio = 100
+        cocheK1.promocion = 5
+
+
+
+        Dim cocheX2 As Coche
+        cocheX2.modelo = "X2"
+        cocheX2.CC = "30"
+        cocheX2.CV = "40"
+        cocheX2.precio = 200
+        cocheX2.promocion = 5
+
+        Dim cocheX3 As Coche
+        cocheX3.modelo = "X3"
+        cocheX3.CC = "30"
+        cocheX3.CV = "40"
+        cocheX3.precio = 300
+        cocheX3.promocion = 5
+
+        listadoBMW(0) = cocheK1
+        listadoBMW(1) = cocheX2
+        listadoBMW(2) = cocheX3
+
+
+        Dim cocheSorento As Coche
+        cocheSorento.modelo = "Sorento"
+        cocheSorento.CC = "50"
+        cocheSorento.CV = "60"
+        cocheSorento.precio = 300
+        cocheSorento.promocion = 5
+
+
+        Dim cocheSoul As Coche
+        cocheSoul.modelo = "Soul"
+        cocheSoul.CC = "500"
+        cocheSoul.CV = "600"
+        cocheSoul.precio = 3000
+        cocheSoul.promocion = 5
+
+        listadoKIA(0) = cocheSorento
+        listadoKIA(1) = cocheSoul
+
+
+
+        Dim cocheAlaska As Coche
+        cocheAlaska.modelo = "Alaska"
+        cocheAlaska.CC = "500"
+        cocheAlaska.CV = "600"
+        cocheAlaska.precio = 100
+        cocheAlaska.promocion = 5
+
+        Dim cocheArkana As Coche
+        cocheArkana.modelo = "Arkana"
+        cocheArkana.CC = "10"
+        cocheArkana.CV = "600"
+        cocheArkana.precio = 30000
+        cocheArkana.promocion = 5
+
+        Dim cocheAustral As Coche
+        cocheAustral.modelo = "Austral"
+        cocheAustral.CC = "500"
+        cocheAustral.CV = "6000"
+        cocheAustral.precio = 900
+        cocheAustral.promocion = 5
+
+        listadoRENAULT(0) = cocheAlaska
+        listadoRENAULT(1) = cocheArkana
+        listadoRENAULT(2) = cocheAustral
+
+
+
+
+
+
+    End Sub
+
+    Private Sub ListBoxModelos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBoxModelos.SelectedIndexChanged
+
+
+        Dim seleccionado As Integer = ListBoxModelos.SelectedIndex
+        If seleccionado <> -1 Then
+            'Dependiendo de la marca escojo el el listado pertinente
+            Select Case Marca
+                Case "BMW"
+                    ' En coche Seleccionado guardo el coche con el que trabajare para las imagenes y los precios
+                    cocheSeleccionado = listadoBMW(seleccionado)
+                    Select Case cocheSeleccionado.modelo
+                        Case "X1"
+                            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\bmw_x1.jpg")
+
+                        Case "X2"
+                            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\bmw_x2.jpg")
+
+                        Case "X3"
+                            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\bmw_x3.jpg")
+
+                    End Select
+                Case "KIA"
+                    cocheSeleccionado = listadoKIA(seleccionado)
+                    Select Case cocheSeleccionado.modelo
+                        Case "Sorento"
+                            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\kia_sorento.jpg")
+
+                        Case "Soul"
+                            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\kia_soul.jpg")
+
+
+
+                    End Select
+
+                Case "RENAULT"
+                    cocheSeleccionado = listadoRENAULT(seleccionado)
+                    Select Case cocheSeleccionado.modelo
+                        Case "Alaska"
+                            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\Renault_alaskan.jpg")
+
+                        Case "Arkana"
+                            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\renault_arkana.jpg")
+
+                        Case "Austral"
+                            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\renault_austral.jpg")
+
+
+
+
+
+                    End Select
+
+
+            End Select
+
+            'If Marca = "BMW" Then
+            '    cocheSeleccionado = listadoBMW(seleccionado)
+
+            '    Select Case cocheSeleccionado.modelo
+            '        Case "X1"
+            '            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\bmw_x1.jpg")
+
+            '        Case "X2"
+            '            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\bmw_x2.jpg")
+
+            '        Case "X3"
+            '            PictureBoxCentro.Image = Image.FromFile(Application.StartupPath & "\AUTOS\bmw_x3.jpg")
+
+            '    End Select
+
+
+
+            'End If
+
+            ' Lleno los textBox con la infomacion del coche seleccionado
+            TextBoxCC.Text = cocheSeleccionado.CC
+            TextBoxCV.Text = cocheSeleccionado.CV
+            TextBoxPrecio.Text = cocheSeleccionado.precio
+            TextBoxPromocion.Text = cocheSeleccionado.promocion
+
+        End If
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub ButtonRealiza_Click(sender As Object, e As EventArgs) Handles ButtonRealiza.Click
+
+        ' Creo un objeto pedido nuevo 
+        Dim pedidoNuevo As Pedidos
+        ' Cargo los valores en funcion del coche seleccionado
+        pedidoNuevo.pedidoModelo = cocheSeleccionado.modelo
+        pedidoNuevo.pedidoMarca = Marca
+        pedidoNuevo.numPedido = indice
+        'Agrego el pedido nuevo al listado
+        listadoPedidos(indice) = pedidoNuevo
+        'Agrego el pedido al listBox
+        ListBoxPedidos.Items.Add(listadoPedidos(pedidoNuevo.numPedido).numPedido)
+        ' Incremento el indice para otro pedido
+        indice = indice + 1
+
+        MsgBox("El precio con el descuento es: " & cocheSeleccionado.precio - cocheSeleccionado.precio * cocheSeleccionado.promocion / 100)
+
+
+
+    End Sub
+
+    Private Sub ButtonVerPedi_Click(sender As Object, e As EventArgs) Handles ButtonVerPedi.Click
+
+        Dim indicedelPedido = ListBoxPedidos.SelectedIndex
+
+        MsgBox("Marca: " & listadoPedidos(indicedelPedido).pedidoMarca & " Modelo: " & listadoPedidos(indicedelPedido).pedidoModelo)
+
+
+    End Sub
+
+    Private Sub ButtonBorrarP_Click(sender As Object, e As EventArgs) Handles ButtonBorrarP.Click
+
+        Dim posicion As Integer
+        posicion = ListBoxPedidos.SelectedIndex
+        Dim eliminado As Integer = posicion
+
+        ListBoxPedidos.Items.Remove(listadoPedidos(posicion).numPedido)
+
+        'Sobre escribo la posicion del choche eliminado, corriendolo una vez.
+        For i As Integer = eliminado To 2
+            listadoPedidos(i) = listadoPedidos(i + 1)
+        Next
+        ' Borro el indice que se ha liberado del array
+        indice = indice - 1
+
+
+
+    End Sub
+
+    Private Sub ButtonSalir_Click(sender As Object, e As EventArgs) Handles ButtonSalir.Click
+        Close()
+    End Sub
+End Class
